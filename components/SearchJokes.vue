a1b1f737608bda18b7bc0c8feff69455767ab45b
Dracorex-xD <DracorexLee@gmail.com>
2023-05-17 20:04:30 +1000

    Base commit

---
+++ components/SearchJokes.vue
@@ -0,0 +1,25 @@
+<template lang="en">
+  <form @submit.prevent="onSubmit">
+    <input type="text" v-model="text" placeholder="Search Jokes...">
+    <input type="submit" value="Search Jokes"></input>
+  </form>
+</template>
+<script>
+export default {
+  name: "SearchJokes",
+  data() {
+    return {
+      text: ""
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$emit("search-text", this.text)
+      this.text = ""
+    }
+  }
+}
+</script>
+<style>
+
+</style>
